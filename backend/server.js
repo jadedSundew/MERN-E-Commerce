15691f7ec4bbec6572f37ffeb38b89793d2103e0
DizzyReading <daintybeat@gmail.com>
2021-08-14 16:53:42 +0530

    Create sample users in MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,27 @@
 import express from 'express';
+import mongoose from 'mongoose';
 import data from '../frontend/src/data.js';
+import userRouter from './routes/userRoutes.js';
+
+// express app
 
 const app = express();
+
+const dbURI = 'mongodb+srv://polka:teamkrillpolka@ecommerce.tc8kz.mongodb.net/Thriftshop?retryWrites=true&w=majority';
+
+// connect to MongoDB
+
+mongoose
+	.connect(process.env.MONGODB_URL || dbURI, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+		useCreateIndex: true
+	})
+	.then((result) => {
+		app.listen(5000, () => {
+			console.log(`Serve at http://localhost:${port}`);
+		});
+	});
 
 // (Individual) Product Screen
 
@@ -21,12 +41,18 @@ app.get('/api/products', (req, res) => {
 	res.send(data.products);
 });
 
+app.use('/api/users', userRouter);
+
 app.get('/', (req, res) => {
 	res.send('Server is ready!');
 });
 
+app.use((err, req, res, next) => {
+	res.status(500).send(err.message);
+});
+
 const port = process.env.PORT || 5000;
 
-app.listen(5000, () => {
-	console.log(`Serve at http://localhost:${port}`);
-});
+// app.listen(5000, () => {
+// 	console.log(`Serve at http://localhost:${port}`);
+// });
--- frontend/src/data.js
+++ frontend/src/data.js
@@ -1,4 +1,20 @@
+import bcrypt from 'bcryptjs';
+
 const data = {
+	users: [
+		{
+			name: 'polka',
+			email: 'admin@example.com',
+			password: bcrypt.hashSync('teamkrill', 8),
+			isAdmin: true
+		},
+		{
+			name: 'Jane',
+			email: 'doe@example.com',
+			password: bcrypt.hashSync('1234', 8),
+			isAdmin: false
+		}
+	],
 	products: [
 		{
 			_id: '1',
